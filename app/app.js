c763566388a0400fa51643f73d6b860d5e36f8aa
choirool <khoiroelf@gmail.com>
2021-08-25 08:08:42 +0800

    Add search airport

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 import Vue from 'nativescript-vue'
 import WebViewPlugin from '@nativescript-community/ui-webview/vue';
+import RadAutoComplete from 'nativescript-ui-autocomplete/vue';
 
 Vue.use(WebViewPlugin);
+Vue.use(RadAutoComplete);
 
 import Home from './components/Home'
 
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,22 +1,30 @@
 <template>
   <Page>
     <ActionBar>
-      <Label text="Home" />
+      <Label text="Airports" />
     </ActionBar>
 
-    <GridLayout columns="*" rows="*">
-      <AWebView
-        col="0"
-        row="0"
-        @loaded="loadStarted"
-        @loadFinished="loadFinished"
-      ></AWebView>
-      <!-- <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; " />
-          <Span :text="message" />
-        </FormattedString>
-      </Label> -->
+    <GridLayout columns="*" rows="50,*">
+      <RadAutoCompleteTextView
+        ref="autocomplete"
+        displayMode="plain"
+        suggestMode="Suggest"
+        :items="dataItems"
+        @didAutoComplete="onDidAutoComplete"
+      >
+        <SuggestionView ~suggestionView suggestionViewHeight="300">
+          <StackLayout
+            v-suggestionItemTemplate
+            orientation="vertical"
+            padding="10"
+          >
+            <v-template>
+              <Label :text="item.text"></Label>
+            </v-template>
+          </StackLayout>
+        </SuggestionView>
+      </RadAutoCompleteTextView>
+      <AWebView col="0" row="1" @loaded="loadStarted"></AWebView>
     </GridLayout>
   </Page>
 </template>
@@ -24,14 +32,52 @@
 <script>
 import { AWebView } from "@nativescript-community/ui-webview";
 import * as geolocation from "@nativescript/geolocation";
+import { TokenModel } from "nativescript-ui-autocomplete";
+import { ObservableArray, Http } from "@nativescript/core";
 
+// https://v6.docs.nativescript.org/vuejs/ns-ui/autocompletetextview/aync-data
 export default {
+  data() {
+    return {
+      dataItems: new ObservableArray(),
+      airports: new Array(),
+      webView: null,
+    };
+  },
+  mounted() {
+    this.loadData();
+  },
   methods: {
-    loadFinished(args) {
-      console.log("load finihsed");
+    loadData() {
+      const jsonUrl =
+        "https://raw.githubusercontent.com/NativeScript/nativescript-ui-samples/master/examples-data/airports.json";
+
+      this.$refs.autocomplete.setLoadSuggestionsAsync((text) => {
+        this.airports = new Array();
+        const promise = new Promise((resolve, reject) => {
+          Http.getJSON(jsonUrl)
+            .then((r) => {
+              const items = new Array();
+              r.airports.forEach((airport) => {
+                items.push(new TokenModel(airport.FIELD2, null));
+                this.airports.push(airport);
+              });
+
+              resolve(items);
+            })
+            .catch((err) => {
+              const message = `Error fetching remote data from ${jsonUrl}: ${err.message}`;
+              console.log(message);
+              reject();
+            });
+        });
+
+        return promise;
+      });
     },
     loadStarted(args) {
       const view = args.object;
+      this.webView = view;
       if (view.android) {
         view.android.getSettings().setAllowFileAccess(true);
         view.android.getSettings().setAllowContentAccess(true);
@@ -66,6 +112,21 @@ export default {
       view.on("zoomin", (data) => {
         console.log(data);
       });
+    },
+    onDidAutoComplete({ text }) {
+      let airport = this.airports.find((airport) => airport.FIELD2 == text);
+      this.updateCoordinate(airport);
+    },
+    updateCoordinate(data) {
+      if (data) {
+        this.webView.emitToWebView("updateCoordinate", {
+          coordinates: {
+            lat: data.FIELD7,
+            lng: data.FIELD8,
+          },
+          zoom: 15,
+        });
+      }
     },
   },
 };
